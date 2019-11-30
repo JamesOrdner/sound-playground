13dace5fb085c09d6e9dff59ab48a2c9c4ccb03a
James Ordner <coasterguy123@gmail.com>
2019-11-30 01:27:40 -0600

    Speaker FFT convolution cleanup

--- src/Audio/Components/ASpeaker.cpp
+++ src/Audio/Components/ASpeaker.cpp
@@ -8,85 +8,94 @@ constexpr size_t blockSize = 64;
 constexpr size_t N		   = blockSize * 2;
 
 ASpeaker::ASpeaker() :
-	prev(0.f),
+	partitions(0),
 	inputBuffer(nullptr),
 	inputPtr(blockSize),
-	inputFFT(nullptr),
-	fdlPtr(0)
-	
+	fftOutput(nullptr),
+	fftPlan(nullptr),
+	fdlPtr(0),
+	ifftInput(nullptr),
+	outputBuffer(nullptr),
+	ifftPlan(nullptr),
+	prev(0.f)
 {
 	bAcceptsInput = false;
 	bAcceptsOutput = true;
+
+	// These should be set up even if wav load fails
+	inputBuffer = fftwf_alloc_real(N);
+	outputBuffer = fftwf_alloc_real(N);
+	std::fill_n(inputBuffer, N, 0.f);
+	std::fill_n(outputBuffer + blockSize, blockSize, 0.f);
 
 	SDL_AudioSpec s;
 	Uint8* data;
 	Uint32 n;
 	if (!SDL_LoadWAV("res/sound/auratone_8192.wav", &s, &data, &n)) {
 		fprintf(stderr, "Could not open wav: %s\n", SDL_GetError());
+		return;
 	}
-	else {
-		SDL_assert(s.format == AUDIO_S16);
+	if (s.format != AUDIO_S16) {
+		fprintf(stderr, "Invalid wav format! Expected signed 16 bit.\n");
+		return;
+	}
 
-		partitions = s.samples / blockSize;
+	partitions = s.samples / blockSize;
 
-		float* fftIn = fftwf_alloc_real(N);
-		std::fill(fftIn + blockSize, fftIn + N, 0.f); // zero padding
+	float* fftIn = fftwf_alloc_real(N);
+	std::fill(fftIn + blockSize, fftIn + N, 0.f); // zero padding
 
-		fftwf_complex* fftOut = fftwf_alloc_complex(N);
-		fftwf_plan p = fftwf_plan_dft_r2c_1d(N, fftIn, fftOut, FFTW_ESTIMATE);
+	fftwf_complex* fftOut = fftwf_alloc_complex(N);
+	fftwf_plan p = fftwf_plan_dft_r2c_1d(N, fftIn, fftOut, FFTW_ESTIMATE);
 
-		ir.reserve(partitions);
-		inputFDL.reserve(partitions);
+	ir.reserve(partitions);
+	freqDelayLine.reserve(partitions);
 
-		Sint16* sData = (Sint16*) data;
-		for (size_t i = 0; i < partitions; i++) {
-			size_t offset = i * blockSize;
-			for (size_t j = 0; j < blockSize; j++) {
-				fftIn[j] = static_cast<float>(sData[offset + j]) / SDL_MAX_SINT16;
-			}
-			
-			fftwf_execute(p);
-
-			fftwf_complex* arr = fftwf_alloc_complex(N);
-			std::copy_n((float*)fftOut, 2 * N, (float*)arr);
-			ir.push_back(arr);
-
-			// Create input FDL buffers
-			fftwf_complex* fdlBuffer = fftwf_alloc_complex(N);
-			std::fill((float*) fdlBuffer, (float*) (fdlBuffer + N), 0.f);
-			inputFDL.push_back(fdlBuffer);
+	Sint16* sData = (Sint16*) data;
+	for (size_t i = 0; i < partitions; i++) {
+		size_t offset = i * blockSize;
+		for (size_t j = 0; j < blockSize; j++) {
+			fftIn[j] = static_cast<float>(sData[offset + j]) / SDL_MAX_SINT16;
 		}
+			
+		fftwf_execute(p);
 
-		fftwf_destroy_plan(p);
-		fftwf_free(fftIn);
-		fftwf_free(fftOut);
-		SDL_FreeWAV(data);
+		fftwf_complex* arr = fftwf_alloc_complex(blockSize + 1);
+		std::copy_n(&fftOut[0][0], 2 * (blockSize + 1), &arr[0][0]);
+		ir.push_back(arr);
 
-		// Input FFT
-		inputBuffer = fftwf_alloc_real(N);
-		inputFFT = fftwf_alloc_complex(N);
-		inputPlan = fftwf_plan_dft_r2c_1d(N, inputBuffer, inputFFT, FFTW_ESTIMATE);
-		std::fill(inputBuffer, inputBuffer + N, 0.f);
-
-		// Output IFFT
-		ifftInput = fftwf_alloc_complex(N);
-		outputBuffer = fftwf_alloc_real(N);
-		ifftPlan = fftwf_plan_dft_c2r_1d(N, ifftInput, outputBuffer, FFTW_ESTIMATE);
-		std::fill(outputBuffer + blockSize, outputBuffer + N, 0.f);
+		// Create input FDL buffers
+		fftwf_complex* fdlBuffer = fftwf_alloc_complex(blockSize + 1);
+		std::fill_n(&fdlBuffer[0][0], 2 * (blockSize + 1), 0.f);
+		freqDelayLine.push_back(fdlBuffer);
 	}
+
+	fftwf_destroy_plan(p);
+	fftwf_free(fftIn);
+	fftwf_free(fftOut);
+	SDL_FreeWAV(data);
+
+	// Input FFT
+	fftOutput = fftwf_alloc_complex(N);
+	fftPlan = fftwf_plan_dft_r2c_1d(N, inputBuffer, fftOutput, FFTW_ESTIMATE);
+
+	// Output IFFT
+	ifftInput = fftwf_alloc_complex(N);
+	std::fill(&(ifftInput + blockSize + 1)[0][0], &(ifftInput + N)[0][0], 0.f);
+	ifftPlan = fftwf_plan_dft_c2r_1d(N, ifftInput, outputBuffer, FFTW_ESTIMATE);
 }
 
 ASpeaker::~ASpeaker()
 {
-	fftwf_destroy_plan(inputPlan);
-	fftwf_destroy_plan(ifftPlan);
+	if (fftPlan) fftwf_destroy_plan(fftPlan);
+	if (ifftPlan) fftwf_destroy_plan(ifftPlan);
 	if (inputBuffer) fftwf_free(inputBuffer);
-	if (inputFFT) fftwf_free(inputFFT);
+	if (fftOutput) fftwf_free(fftOutput);
 	if (ifftInput) fftwf_free(ifftInput);
 	if (outputBuffer) fftwf_free(outputBuffer);
 
 	for (fftwf_complex* c : ir) fftwf_free(c);
-	for (fftwf_complex* c : inputFDL) fftwf_free(c);
+	for (fftwf_complex* c : freqDelayLine) fftwf_free(c);
 }
 
 size_t ASpeaker::process(size_t n)
@@ -95,41 +104,42 @@ size_t ASpeaker::process(size_t n)
 	if (p < n) n = p;
 	size_t size = ir.size();
 	for (const auto& output : outputs) {
-		//float gain = calcGain(output->dest.lock()->position());
+		float gain = calcGain(output->dest.lock()->position());
 		for (size_t i = 0; i < n; i++) {
 			float out = outputBuffer[inputPtr] / N;
-			output->buffer.push(out);
-
 			float s = static_cast<float>(rand()) / RAND_MAX * 2.f - 1.f;
 			inputBuffer[inputPtr++] = s;
-			if (inputPtr == N) {
-				// perform fft of input
-				fftwf_execute(inputPlan);
-				fftwf_complex* arr = inputFDL[fdlPtr++];
-				std::copy_n((float*)inputFFT, 2 * N, (float*)arr);
-				if (fdlPtr == inputFDL.size()) fdlPtr = 0;
 
-				// perform pointwise multiply and add of all FDL blocks
-				std::fill((float*)ifftInput, (float*)(ifftInput + N), 0.f);
-				for (size_t partition = 0; partition < partitions; partition++) {
-					size_t p = (fdlPtr + partition) % partitions; // start at oldest FDL block
-					for (size_t n = 0; n < N; n++) {
-						for (size_t k = 0; k < 2; k++) {
-							size_t irIdx = partitions - partition - 1; // last to first
-							ifftInput[n][k] += inputFDL[p][n][k] * ir[irIdx][n][k];
+			if (inputPtr == N) {
+				inputPtr = blockSize;
+				if (fftPlan) {
+					// perform fft of input
+					fftwf_execute(fftPlan);
+					fftwf_complex* arr = freqDelayLine[fdlPtr++];
+					std::copy_n(&fftOutput[0][0], 2 * (blockSize + 1), &arr[0][0]);
+					if (fdlPtr == freqDelayLine.size()) fdlPtr = 0;
+
+					// perform pointwise multiply and add of all FDL blocks
+					std::fill_n((float*)ifftInput[0], 2 * (blockSize + 1), 0.f);
+					for (size_t partition = 0; partition < partitions; partition++) {
+						size_t p = (fdlPtr + partition) % partitions; // start at oldest FDL block
+						for (size_t n = 0; n <= blockSize; n++) {
+							for (size_t k = 0; k < 2; k++) {
+								size_t irIdx = partitions - partition - 1; // last to first
+								ifftInput[n][k] += freqDelayLine[p][n][k] * ir[irIdx][n][k];
+							}
 						}
 					}
-				}
-				fftwf_execute(ifftPlan);
+					fftwf_execute(ifftPlan);
 
-				// shift samples one block to the left and reset inputPtr
-				std::copy_n(inputBuffer + blockSize, blockSize, inputBuffer);
-				inputPtr = blockSize;
+					// shift samples one block to the left
+					std::copy_n(inputBuffer + blockSize, blockSize, inputBuffer);
+				}
 			}
 
-			//float filtered = (out + prev * (1.f - gain)) / (2.f / (1.f + gain)) * (gain * 0.5f + 0.5f);
-			//output->buffer.push(filtered);
-			//prev = out;
+			float filtered = (out + prev * (1.f - gain)) / (2.f / (1.f + gain)) * (gain * 0.5f + 0.5f);
+			output->buffer.push(filtered);
+			prev = out;
 		}
 	}
 	return n;
--- src/Audio/Components/ASpeaker.h
+++ src/Audio/Components/ASpeaker.h
@@ -19,35 +19,35 @@ private:
 	// Number of FFT partitions, dependent on impulse response length
 	size_t partitions;
 
-	// Stores input samples to be used as input to fft
+	// Partitioned, complex frequency-domain impulse response blocks
+	std::vector<fftwf_complex*> ir;
+
+	// Stores input samples to be used as input to FFT
 	float* inputBuffer;
 
 	// Points to the next write index in inputBuffer
 	int inputPtr;
 
-	// Stores output of input fft
-	fftwf_complex* inputFFT;
+	// Stores output of input FFT
+	fftwf_complex* fftOutput;
 
-	// Input fft plan
-	fftwf_plan inputPlan;
+	// Input FFT plan
+	fftwf_plan fftPlan;
 
 	// Partitioned, complex input frequency-domain delay line
-	std::vector<fftwf_complex*> inputFDL;
+	std::vector<fftwf_complex*> freqDelayLine;
 
 	// Points to the next write index in inputFDL
 	int fdlPtr;
 
-	// Partitioned, complex frequency-domain impulse response slices
-	std::vector<fftwf_complex*> ir;
-
 	// Input to the final IFFT after all multiply and add operations
 	fftwf_complex* ifftInput;
 
-	// Final IFFT plan
-	fftwf_plan ifftPlan;
-
 	// Stores final output samples to be sent to output
 	float* outputBuffer;
+
+	// Final IFFT plan
+	fftwf_plan ifftPlan;
 
 	// Previous sample, saved for simple directional lpf
 	float prev;
