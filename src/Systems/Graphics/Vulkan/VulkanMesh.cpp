ef7f36e7b3211d29f615fcff5441482c838db945
JamesOrdner <coasterguy123@gmail.com>
2020-03-02 15:50:38 -0600

    Consolidate vertex attribute indices and stride into single enum

--- src/Systems/Graphics/Vulkan/VulkanMesh.cpp
+++ src/Systems/Graphics/Vulkan/VulkanMesh.cpp
@@ -1,24 +1,14 @@
 #include "VulkanMesh.h"
 
-enum class VertexAttributeType {
-    Position,
-    Color
+/// VertexAttribute enum values are equal to the attribute offset within the vertex buffer
+enum class VertexAttribute : size_t
+{
+    Position = 0,
+    Color = 3,
+	
+	/// Stride of all attributes within the vertex buffer
+	IndexStride = 6
 };
-
-inline size_t attributeIndexOffset(VertexAttributeType attribute)
-{
-    switch (attribute) {
-    case VertexAttributeType::Position: return 0;
-    case VertexAttributeType::Color:    return 3;
-    default:
-        return 0;
-    }
-}
-
-inline size_t attributeIndexStride()
-{
-    return 6;
-}
 
 VulkanMesh::VulkanMesh(const std::string& filepath) :
 	filepath(filepath)
@@ -29,7 +19,7 @@ VkVertexInputBindingDescription VulkanMesh::inputBindingDescription()
 {
     return VkVertexInputBindingDescription{
         .binding = 0,
-        .stride = static_cast<uint32_t>(attributeIndexStride() * sizeof(float)),
+        .stride = static_cast<uint32_t>(static_cast<size_t>(VertexAttribute::IndexStride) * sizeof(float)),
         .inputRate = VK_VERTEX_INPUT_RATE_VERTEX
     };
 }
@@ -40,14 +30,14 @@ std::vector<VkVertexInputAttributeDescription> VulkanMesh::inputAttributeDescrip
         .location = 0,
         .binding = 0,
         .format = VK_FORMAT_R32G32B32_SFLOAT,
-        .offset = static_cast<uint32_t>(attributeIndexOffset(VertexAttributeType::Position) * sizeof(float))
+        .offset = static_cast<uint32_t>(static_cast<size_t>(VertexAttribute::Position) * sizeof(float))
     };
 
     VkVertexInputAttributeDescription color{
         .location = 1,
         .binding = 0,
         .format = VK_FORMAT_R32G32B32_SFLOAT,
-        .offset = static_cast<uint32_t>(attributeIndexOffset(VertexAttributeType::Color) * sizeof(float))
+        .offset = static_cast<uint32_t>(static_cast<size_t>(VertexAttribute::Color) * sizeof(float))
     };
 
     return { position, color };
